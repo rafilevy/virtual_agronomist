d420c8f6638464c4953131b26c272272f344fc22
Rafi Levy <rafi.levy@sky.com>
2021-02-09 11:24:14 +0000

    Fixed root div stylings

--- infrastructure/frontend/js/components/App.tsx
+++ infrastructure/frontend/js/components/App.tsx
@@ -9,6 +9,7 @@ const useStyles = makeStyles((theme)=> ({
     root: {
         width: "100%",
         height: "100%",
+        padding: "20px",
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-evenly"
