5c5fe3b32651ab9341826dcb5779d2ed81d77c48
Aravind Prabhakaran <ap2138@cam.ac.uk>
2021-02-25 12:32:03 +0000

    changed elasticsearch version

--- infrastructure/backend/training/DPRTrainingSet.py
+++ infrastructure/backend/training/DPRTrainingSet.py
@@ -22,17 +22,16 @@
 # #############
 
 
-
-
-
-import json # for outputting training dataset
-import random # for adding in batch negatives
+import json  # for outputting training dataset
+import random  # for adding in batch negatives
 
 # the individidual data points to train
+
+
 class DPRTrainingItem:
 
     # populate the data point with inital data
-    def __init__(self,question,answer,positive_ctx,hard_negative_ctxs):
+    def __init__(self, question, answer, positive_ctx, hard_negative_ctxs):
         self.dict_DPR = {
             "question": question,
             "answers": [answer],
@@ -47,17 +46,19 @@ class DPRTrainingItem:
 
     # add in batch negative from other points
     # return false if tried to add the positive context as a negative one
-    def addNegativeCtx(self,c):
-        if c["passage_id"]==self.getPositiveCtx()["passage_id"]:
-            return False;
+    def addNegativeCtx(self, c):
+        if c["passage_id"] == self.getPositiveCtx()["passage_id"]:
+            return False
         else:
             self.dict_DPR["hard_negative_ctxs"].append(c)
-            return True;
+            return True
 
 # contains set of dpr training items
 # used to add training points and create json training file
+
+
 class DPRTrainingSet:
-    def __init__(self, document_store,round):
+    def __init__(self, document_store, round):
         self.document_store = document_store
         self.round = round
         self.trainingSet = []
@@ -68,7 +69,7 @@ class DPRTrainingSet:
             "title": "",
             "text": "",
             "score": 0,
-            "title_score": None, #what on earth to with this....
+            "title_score": None,  # what on earth to with this....
             "passage_id": ""
         }
         context["title"] = document.meta['name']
@@ -77,15 +78,15 @@ class DPRTrainingSet:
         context["passage_id"] = document.id
         return context
 
-    def addItem(self,question,posID,negIDs):
+    def addItem(self, question, posID, negIDs):
         # create dpr context from the id of the correct document
-        positive_context = self.getContext(posID)#
+        positive_context = self.getContext(posID)
         # extract the text as this is the correct answer
         answer = positive_context["text"]
 
         # create a list of all the contexts from incorrect documents
         negative_contexts = []
-        for i in range(0,len(negIDs)):
+        for i in range(0, len(negIDs)):
             negative_contexts.append(self.getContext(negIDs[i]))
 
         newItem = DPRTrainingItem(
@@ -104,22 +105,22 @@ class DPRTrainingSet:
             N = len(self.trainingSet)
 
         for i in self.trainingSet:
-            for j in range(0,N):
-                neg = random.randint(0,len(self.trainingSet)-1)
+            for j in range(0, N):
+                neg = random.randint(0, len(self.trainingSet) - 1)
                 i.addNegativeCtx(self.trainingSet[neg].getPositiveCtx())
 
         # self.printSet()
 
-    def generateJSON(self,saveFile):
+    def generateJSON(self, saveFile):
         bigList = []
         for i in self.trainingSet:
             bigList.append(i.dict_DPR)
 
         items = [json.dumps(dataPoint, indent=6) for dataPoint in bigList]
         textOutput = "[%s]" % ",\n".join(items)
-        open(saveFile,'w').write(textOutput)
+        open(saveFile, 'w').write(textOutput)
 
     def printSet(self):
-        for i in range(0,len(self.trainingSet)):
+        for i in range(0, len(self.trainingSet)):
             print(self.trainingSet[i].dict_DPR)
             print()
